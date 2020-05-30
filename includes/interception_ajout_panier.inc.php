f61717fade1f92159ad609d98bc5464bd54236fa
user <user@DESKTOP-4N4JKEG>
2020-05-30 17:05:35 +0200

    Premier fix sur les btn ajouter au panier Ajout de css pour les boutons ajouter au panier

--- includes/interception_ajout_panier.inc.php
+++ includes/interception_ajout_panier.inc.php
@@ -1,4 +1,7 @@
 <?php
+//Je récupère l'id du casque ajouté au panier à l'aide de l'url
+//puis je l'ajoute à mon array $_SESSION['panier']
+//Je ne pense pas que ce soit une bonne méthode mais c'est la seule que j'ai trouvé
 $url=filter_input(INPUT_SERVER,'REQUEST_URI');
 $pos=strpos($url,'=');
 if ($pos!==false) {
