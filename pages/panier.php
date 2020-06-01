84c0a2b6aa12a6c63139dbc7fbcf3e4d03e25f34
user <user@DESKTOP-4N4JKEG>
2020-06-01 20:05:09 +0200

    Mise en forme du panier

--- pages/panier.php
+++ pages/panier.php
@@ -16,14 +16,47 @@ include('../includes/debut_page.php')
     <body>
         <?php
         include('../includes/header.html.inc.php');
-        
-        if(empty($_SESSION['panier'])){
+
+        if (empty($_SESSION['panier'])) {
             echo "<p>Votre panier est vide</p>";
+        } else {
+            //echo var_dump($_SESSION['panier']);
+            echo '<section id="panier">';
+            $cout = 0;
+            foreach ($_SESSION['panier'] as $casque) {
+                $cnx = new PDO('mysql:host=127.0.0.1;dbname=nolark', 'nolarkuser', 'nolarkpwd');
+                $req = 'select nom, modele, prix, image, libelle '
+                        . 'from casque '
+                        . 'inner join marque on casque.marque=marque.id '
+                        . 'inner join type on casque.type=type.id '
+                        . 'where casque.id=' . $casque;
+                $res = $cnx->prepare($req);
+                $res->execute();
+                $article = $res->fetch();
+                unset($cnx);
+                if (!empty($article['prix'])) {
+                    $cout += $article['prix'];
+                    echo'<article class="casque_panier">'
+                    . '<img src="../images/casques/' . $article['libelle'] . '/' . $article['image'] . '" alt="' . $article['modele'] . '">'
+                    . '<p class="prix_article">' . $article['prix'] . '€</p>'
+                    . '<p class="nom_article">' . $article['nom'] . '</p>'
+                    . '<p class="modele_article">' . $article['modele'] . '</p>'
+                    . '</article>';
+                } else {
+                    unset($_SESSION['panier'][array_search($casque, $_SESSION['panier'])]);
+                    echo '<p class="error">Article indisponible</p>';
+                }
+            }
+            echo'</section>';
+            echo'<section id="total_panier">'
+            . '<p id="nb_articles">Nombre d\'articles : ' . count($_SESSION['panier']) . '</p>'
+            . '<p id=cout_articles>Coût total des articles : ' . $cout . '€</p>'
+            . '<p id="cout_livraison">Coût de la livraison : 0.00€</p>'
+            . '<p id="cout_total">Total : ' . $cout . '€</p>'
+            . '</section>';
+             
         }
-        else{
-            echo var_dump($_SESSION['panier']);
-        }
-        
+
         include('../includes/footer.inc.php');
         ?>
     </body>
